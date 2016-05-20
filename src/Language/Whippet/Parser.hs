df3ae15e3b4a7d40e180b64be5f6175b955cb48b
Chris Barrett <chris.d.barrett@me.com>
2016-05-21 09:42:23 +1200

    Implement parsing for empty signatures

--- src/Language/Whippet/Parser.hs
+++ src/Language/Whippet/Parser.hs
@@ -2,19 +2,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Language.Whippet.Parser where
 
+import           Control.Applicative
 import           Control.Monad.Trans     (MonadIO)
 import           Language.Whippet.AST
 import           Text.Parser.Token.Style
 import           Text.Trifecta
 
 parseFile :: MonadIO m => FilePath -> m (Result (AST Span))
-parseFile = parseFromFileEx parseModule
+parseFile = parseFromFileEx (module' <|> signature)
 
-parseModule :: Parser (AST Span)
-parseModule = do
+module' :: Parser (AST Span)
+module' = do
     let parser = reserved "module" *> ((,) <$> identifier <*> braces decls)
     ((id, ds) :~ span) <- spanned parser
     pure (AstModule span id ds)
+
+signature :: Parser (AST Span)
+signature = do
+    let parser = reserved "signature" *> ((,) <$> identifier <*> braces decls)
+    ((id, ds) :~ span) <- spanned parser
+    pure (AstSignature span id ds)
 
 identifier :: Parser Ident
 identifier = Ident <$> ident style
@@ -26,4 +33,4 @@ reserved :: String -> Parser ()
 reserved = reserve style
 
 style = emptyIdents {_styleReserved = reservedChars}
-reservedChars = ["module"]
+reservedChars = ["module", "signature"]
