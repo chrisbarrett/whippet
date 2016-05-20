2522d082036cfb37d7cf7a65b4d0ff5b85fbf8ce
Chris Barrett <chris.d.barrett@me.com>
2016-05-21 00:54:15 +1200

    Parse empty modules

--- src/Language/Whippet/Parser.hs
+++ src/Language/Whippet/Parser.hs
@@ -1,11 +1,29 @@
+{-# LANGUAGE OverloadedLists   #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Language.Whippet.Parser where
 
-import           Control.Monad.Trans  (MonadIO)
+import           Control.Monad.Trans     (MonadIO)
 import           Language.Whippet.AST
+import           Text.Parser.Token.Style
 import           Text.Trifecta
 
 parseFile :: MonadIO m => FilePath -> m (Result (AST Span))
 parseFile = parseFromFileEx parseModule
 
 parseModule :: Parser (AST Span)
-parseModule = undefined
+parseModule = do
+    let parser = reserved "module" *> ((,) <$> identifier <*> braces decls)
+    ((id, ds) :~ span) <- spanned parser
+    pure (AstModule span id ds)
+
+identifier :: Parser Ident
+identifier = Ident <$> ident style
+
+decls :: Parser [Decl Span]
+decls = pure []
+
+reserved :: String -> Parser ()
+reserved = reserve style
+
+style = emptyIdents {_styleReserved = reservedChars}
+reservedChars = ["module"]
